d840586674bcffb13fc13dfef04b54eb6337a52c
LeonardoAugusto9397 <leonardo.augusto.93@outlook.com>
2022-09-25 13:32:27 -0300

    Padronização das páginas, criação da página documentação e fórum

--- js/accessibility-dark-mode.js
+++ js/accessibility-dark-mode.js
@@ -1,29 +1,28 @@
+//DARK MODE
 const chk = document.getElementById('chk')
-
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark-mode')
-})
+});
 
-function aumenta(){
+function aumenta() {
     let el = document.querySelector('html');
     // alterando uma propriedade
     el.style.fontSize = '20px';
 }
-function diminui(){
+function diminui() {
     let el = document.querySelector('html');
     // alterando uma propriedade
     el.style.fontSize = '15px';
 }
-function dislexia(){
+function dislexia() {
     let el = document.querySelector('body');
     // alterando uma propriedade
     el.style.fontFamily  = 'opendyslexicaltaregular';
     el.style.fontSize    = '15px';
 }
-function roboto(){
+function roboto() {
     let el = document.querySelector('body');
     // alterando uma propriedade
     el.style.fontFamily  = 'roboto';
     el.style.fontSize    = '15px';
 }
-
